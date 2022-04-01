c3ebaf70877764e511b1246d83d2e5946040056b
FabreVictor <Victor.fabre@lahiette.com>
2022-04-01 08:06:51 +0200

    Update game-main.js

--- game-main.js
+++ game-main.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 800,
+    width: 600,
+    height: 600,
     physics: {
         default: 'arcade',
         arcade: {
@@ -24,30 +24,83 @@ new Phaser.Game(config);
 
 function preload() {
     this.load.image('sky', 'assets/sky.png')
+    this.load.image('blueshot', 'assets/tirbleu.png')
+    this.load.image('greenshot', 'assets/tirver.png')
+    this.load.image('redshot', 'assets/tirrouge.png')
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('perso', 'assets/perso.png', {
+
+    this.load.spritesheet('foot', 'assets/foot.png', {
         frameWidth: 32,
-        frameHeight: 48
+        frameHeight: 32
     })
+    this.load.spritesheet('persoiddle', 'assets/astromarin.png', {
+        frameWidth: 32,
+        frameHeight: 32
+    });
+    let perso = this.load.spritesheet('perso', 'assets/spriteastromarin.png', {
+        frameWidth: 32,
+        frameHeight: 32
+    })
+    this.load.spritesheet('monstre', 'assets/monstre.png', {
+        frameWidth: 32,
+        frameHeight: 32
+    })
+
+
 }
 
 /*function create(){
   this.add.image(400, 300, 'sky');
   this.add.image(400, 300, 'star');
-
   //game.tKey = game.input.keyboard.addKey( 't')
 }*/
+let weaponsList = ["rifle", "machinegun", "lasercutter"]
+let weaponsDef = {
+    "rifle": {
+        currentAmmo: 15,
+        ammoBase: 15,
+        image: "...../png",
+        bullet: "blueshot",
+        bullet_speed: 2,
+        cadence: 50,
+        lifespan: 1000,
+        damage: 2,
+        maxShot: 100,
+
+    },
+    "machinegun": {
+        currentAmmo: 150,
+        ammoBase: 150,
+        image: "...../png",
+        bullet: "greenshot",
+        bullet_speed: 2,
+        cadence: 50,
+        lifespan: 1000,
+        damage: 2,
+        maxShot: 100,
+    },
+    "lasercutter": {
+        currentAmmo: 10000,
+        ammoBase: 10000,
+        image: "...../png",
+        bullet: "redshot",
+        bullet_speed: 2,
+        cadence: 1,
+        lifespan: 75,
+        damage: 2,
+        maxShot: 5000,
+    },
+}
 
 var platforms;
-var light
+var light;
 var player;
 var cursors;
 var stars;
-var ammo;
-ammo = 10
-var bombs = false;
+var rifleAmmo = 1500;
+var monstres = false;
 var gameOver = false;
 var spacebar;
 var keyT;
@@ -62,15 +115,16 @@ var lastFired = 0;
 var isDown = false;
 var mouseX = 0;
 var mouseY = 0;
+var UICam
 
 var playerSpeed
-playerSpeed = 200
+playerSpeed = 100
 var maxShoot
-maxShoot = 50
+maxShoot = 5000
 var lifeShoot
-lifeShoot = 1000
+lifeShoot = 75
 var cadenceShoot
-cadenceShoot = 100
+cadenceShoot = 0
 
 
 
@@ -99,59 +153,99 @@ function checkJump(player) {
     }
 }
 
+function updatePlayerWeapon(player) {
+    player.weaponName = weaponsList[player.weaponIndex]
+    player.weapon = Object.assign({}, weaponsDef[player.weaponName])
+}
+
 function create() {
 
+
     let sky = this.add.image(400, 300, 'sky')
-
-    const spectrum = Phaser.Display.Color.ColorSpectrum(128)
-    let radius = 128
-    let intensity = 1
-    let colorIndex = 12
-    light = this.add.pointlight(400, 300, 0, radius, intensity)
-    let color = spectrum[colorIndex]
-    light.color.setTo(color.r, color.g, color.b)
-
     platforms = this.physics.add.staticGroup();
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
-    player = this.physics.add.sprite(100, 450, 'perso');
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-    this.physics.add.collider(player, platforms);
+
+    // Player definition
+    player = this.physics.add.sprite(100, 450, 'perso').setScale(1.5).refreshBody();
+    player.setBounce(0)
+    player.setCollideWorldBounds(true)
+    player.setDepth(10)
+    player.weaponIndex = 0
+    updatePlayerWeapon(player)
+
+    this.physics.add.collider(player, platforms)
+
     this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('perso', {
+        key: 'monstreMove',
+        frames: this.anims.generateFrameNumbers('monstre', {
             start: 0,
             end: 3
         }),
-        frameRate: 10,
+        frameRate: 3,
         repeat: -1
     });
     this.anims.create({
-        key: 'turn',
-        frames: [{
-            key: 'perso',
-            frame: 4
-        }],
-        frameRate: 20
+        key: 'footside',
+        frames: this.anims.generateFrameNumbers('foot', {
+            start: 0,
+            end: 15
+        }),
+        frameRate: 10,
+        repeat: -1
+
     });
     this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('perso', {
-            start: 5,
-            end: 8
+        key: 'footup',
+        frames: this.anims.generateFrameNumbers('foot', {
+            start: 16,
+            end: 31
         }),
         frameRate: 10,
         repeat: -1
     });
-    cursors = this.input.keyboard.createCursorKeys();
-    scoreText = this.add.text(16, 16, 'ammo : ' + ammo, {
+    this.anims.create({
+        key: 'footiddle',
+        frames: this.anims.generateFrameNumbers('foot', {
+            start: 0,
+            end: 0
+        }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'moving',
+        frames: this.anims.generateFrameNumbers('perso', {
+            start: 0,
+            end: 7
+        }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'iddle',
+        frames: this.anims.generateFrameNumbers('persoiddle', {
+            start: 0,
+            end: 0
+        }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+
+    cursors = this.input.keyboard.createCursorKeys()
+
+    //affiche un texte à l’écran, pour le score
+    // Text ammo 
+    scoreText = this.add.text(16, 16, (player.weaponName + 'Ammo: ' + player.weapon.currentAmmo), {
         fontSize: '32px',
         fill: '#000'
-    });
-    //affiche un texte à l’écran, pour le score
+    })
+    scoreText.setColor('white')
+    scoreText.setScrollFactor(1)
+
     stars = this.physics.add.group({
         key: 'star',
         repeat: 11,
@@ -171,10 +265,9 @@ function create() {
     this.physics.add.overlap(player, stars, collectStar, null, this);
     //le contact perso/étoile ne génère pas de collision (overlap)
     //mais en revanche cela déclenche une fonction collectStar
-    bombs = this.physics.add.group();
-    this.physics.add.collider(bombs, platforms);
-    this.physics.add.collider(player, bombs, hitBomb, null, this);
-
+    monstres = this.physics.add.group();
+    this.physics.add.collider(monstres, platforms);
+    this.physics.add.collider(player, monstres, hitMonstre, null, this);
 
 
     this.input.keyboard.enabled = true
@@ -186,32 +279,38 @@ function create() {
     keyS = this.input.keyboard.addKey('s');
     doubleJump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
 
+    player.playerFoot = this.physics.add.sprite(100, 450, 'foot').setScale(1.5).refreshBody();
+    player.setBounce(0)
+    player.setCollideWorldBounds(true)
+    player.playerFoot.setDepth(0)
+
     var Bullet = new Phaser.Class({
         Extends: Phaser.GameObjects.Image,
         initialize: function Bullet(scene) {
-            Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bomb');
+            Phaser.GameObjects.Image.call(this, scene, 0, 0, player.weapon.bullet)
 
-            this.incX = 0;
-            this.incY = 0;
-            this.lifespan = 0;
+            this.incX = 0
+            this.incY = 0
+            this.lifespan = 0
 
-            this.speed = Phaser.Math.GetSpeed(600, 1);
+            this.speed = Phaser.Math.GetSpeed(600, 1)
         },
         fire: function(x, y) {
-            this.setActive(true);
-            this.setVisible(true);
+            this.setTexture(player.weapon.bullet)
+            this.setActive(true)
+            this.setVisible(true)
 
             //  Bullets fire from the middle of the screen to the given x/y
-            this.setPosition(player.x, player.y);
 
-            var angle = Phaser.Math.Angle.Between(x, y, player.x, player.y);
+            var angle = Phaser.Math.Angle.Between(x, y, player.x - player.xcamera, player.y - player.ycamera)
+            this.setRotation(angle)
 
-            this.setRotation(angle);
+            this.incX = Math.cos(angle)
+            this.incY = Math.sin(angle)
 
-            this.incX = Math.cos(angle);
-            this.incY = Math.sin(angle);
+            this.setPosition(player.x - (this.incX * 40), player.y - (this.incY * 40))
 
-            this.lifespan = lifeShoot; //porté tir
+            this.lifespan = player.weapon.lifespan; //porté tir
         },
         update: function(time, delta) {
             this.lifespan -= delta;
@@ -227,10 +326,10 @@ function create() {
     })
     bullets = this.physics.add.group({
         classType: Bullet,
-        maxSize: maxShoot, //munition max afficher a l'ecran
+        maxSize: player.weapon.maxShot, //munition max afficher a l'ecran
         runChildUpdate: true
     })
-    this.physics.add.overlap(bullets, bombs, shootBomb, null, this)
+    this.physics.add.overlap(bullets, monstres, shootMonstre, null, this)
     this.physics.add.collider(bullets, platforms, shootWall, null, this);
     this.input.on('pointerdown', function(pointer) {
 
@@ -251,8 +350,14 @@ function create() {
 
         isDown = false;
 
-    });
+    })
+    this.physics.world.setBounds(0, 0, 3200, 600)
+    this.cameras.main.setBounds(0, 0, 3200, 600)
+    this.cameras.main.startFollow(player)
+    this.cameras.main.ignore([scoreText])
 
+    UICam = this.cameras.add(0, 0, 3200, 600)
+    UICam.ignore([sky, platforms, player, player.playerFoot, stars])
 }
 
 //function shootBomb(bullets, platforms) {
@@ -265,27 +370,30 @@ function create() {
 //gameOver = true;
 //}
 
-function hitBomb(player, bomb) {
+function hitMonstre(player, monstre) {
     this.physics.pause();
-    player.setTint(0xff0000);
-    player.anims.play('turn');
+    player.setTint(0xff0000)
     gameOver = true;
+    this.scene.restart()
+    rifleAmmo = 15
+    scoreText.setText(player.weaponName + 'Ammo: ' + player.weapon.baseAmmo)
+
 }
 
 function shootWall(bullet) {
     bullet.lifespan = 0
 }
 
-function shootBomb(bullet, bomb) {
+function shootMonstre(bullet, monstre) {
     console.log("ici")
     bullet.lifespan = 0
-    bomb.disableBody(true, true)
+    monstre.disableBody(true, true)
 }
 
 function collectStar(player, star) {
     star.disableBody(true, true); // l’étoile disparaît
-    ammo += 10; //augmente le score de 10
-    scoreText.setText('Ammo: ' + ammo); //met à jour l’affichage du score 
+    rifleAmmo += 10; //augmente le score de 10
+    scoreText.setText(player.weaponName + 'Ammo: ' + player.weapon.currentAmmo); //met à jour l’affichage du score 
     if (stars.countActive(true) === 0) { // si toutes les étoiles sont prises
         stars.children.iterate(function(child) {
             child.enableBody(true, child.x, 0, true, true);
@@ -294,53 +402,86 @@ function collectStar(player, star) {
             Phaser.Math.Between(400, 400);
         // si le perso est à gauche de l’écran, on met une bombe à droite
         // si non, on la met à gauche de l’écran
-        var bomb = bombs.create(x, 16, 'bomb')
-        bomb.setBounce(1)
-        bomb.setCollideWorldBounds(true)
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20)
-        bomb.allowGravity = false //elle n’est pas soumise à la gravité
+        var monstre = monstres.create(x, 16, 'bomb')
+        monstre.anims.play('monstreMove', true)
+        monstre.setBounce(1)
+        monstre.setCollideWorldBounds(true)
+        monstre.setVelocity(Phaser.Math.Between(-200, 200), 20)
+        monstre.allowGravity = false //elle n’est pas soumise à la gravité
+        UICam.ignore([monstre])
     }
 }
 
 function update(time, delta) {
 
-    // Update light
-    light.x = player.x
-    light.y = player.y
+
+    if (Phaser.Input.Keyboard.JustDown(keyT)) {
+        player.weaponIndex += 1
+        scoreText.setText(player.weaponName + 'Ammo: ' + player.weapon.currentAmmo);
+        if (player.weaponIndex >= weaponsList.length) {
+            player.weaponIndex = 0
+        }
+        updatePlayerWeapon(player)
+    }
 
     if (Phaser.Input.Keyboard.DownDuration(keyZ, 100000)) {
-        player.setVelocityY(-playerSpeed);
-        player.anims.play('right', true);
+        player.setVelocityY(-playerSpeed)
+        player.movingy = true
+        player.playerFoot.anims.play('footup', true)
+        player.anims.play('moving', true)
+
+
     } else if (Phaser.Input.Keyboard.DownDuration(keyS, 100000)) {
-        player.setVelocityY(playerSpeed);
-        player.anims.play('right', true);
+        player.setVelocityY(playerSpeed)
+        player.movingy = true
+        player.playerFoot.anims.play('footup', true)
+        player.anims.play('moving', true)
+
     } else {
-        player.setVelocityY(0);
-        player.anims.play('turn');
+        player.setVelocityY(0)
+        player.movingy = false
     }
     if (Phaser.Input.Keyboard.DownDuration(keyD, 100000)) {
-        player.setVelocityX(playerSpeed);
-        player.anims.play('right', true);
+        player.setVelocityX(playerSpeed)
+        player.movingx = true
+        player.playerFoot.anims.play('footside', true)
+        player.anims.play('moving', true)
+
     } else if (Phaser.Input.Keyboard.DownDuration(keyQ, 100000)) {
-        player.setVelocityX(-playerSpeed);
-        player.anims.play('right', true);
+        player.setVelocityX(-playerSpeed)
+        player.movingx = true
+        player.playerFoot.anims.play('footside', true)
+        player.anims.play('moving', true)
+
     } else {
-        player.setVelocityX(0);
-        player.anims.play('turn');
+        player.setVelocityX(0)
+        player.movingx = false
+    }
+    //console.log(player, player.velocityX, player.velocityY)
+    if (player.movingx == false && player.movingy == false) {
+        console.log("IDDLE")
+        player.anims.play('iddle', true)
+        player.playerFoot.anims.play('footiddle', true)
     }
     if ((isDown && time > lastFired)) {
         var bullet = bullets.get();
+        UICam.ignore([bullet])
 
-        if (bullet && ammo > 0) {
+        if (bullet && player.weapon.currentAmmo > 0) {
             bullet.fire(mouseX, mouseY);
-            ammo--;
-            scoreText.setText('Ammo: ' + ammo); //met à jour l’affichage du score 
-            lastFired = time + cadenceShoot; //duréé de la rafale
+            player.weapon.currentAmmo--;
+            scoreText.setText(player.weaponName + 'Ammo: ' + player.weapon.currentAmmo); //met à jour l’affichage du score 
+            lastFired = time + player.weapon.cadence; //duréé de la rafale
         }
+
     }
 
-    player.setRotation(Phaser.Math.Angle.Between(mouseX, mouseY, player.x, player.y) - Math.PI / 2);
-
+    player.xcamera = Math.floor(this.cameras.main.worldView.x)
+    player.ycamera = Math.floor(this.cameras.main.worldView.y)
+    player.setRotation(Phaser.Math.Angle.Between(mouseX, mouseY, player.x - player.xcamera, player.y - player.ycamera) - Math.PI / 2)
+    player.playerFoot.x = player.x
+    player.playerFoot.y = player.y
+    player.setDepth(10)
 
     /*if (Phaser.Input.Keyboard.JustDown( doubleJump ) && player.body.touching.down){
         //si touche haut appuyée ET que le perso touche le sol
